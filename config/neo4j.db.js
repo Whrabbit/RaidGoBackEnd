894ea01b91c14a2e6eb5b1fd3bc5aade0ad821d1
Whitney <whitneyrabbitc@gmail.com>
2017-12-07 16:55:20 +0100

    neo4j problems

--- config/neo4j.db.js
+++ config/neo4j.db.js
@@ -1,9 +1,13 @@
 const neo4j = require('neo4j-driver').v1;
 
 const neo = {
-    uri: 'bolt://hobby-fombiclecffdgbkeemblpial.dbs.graphenedb.com:24786',
-    username: 'admin',
-    password: 'b.EElh3VqwOSiP.gXiD9H3gtLuKa9MJ'
+    uri: 'bolt://localhost',
+    username: 'neo4j',
+    password: 'password'
+    //
+    // uri: 'bolt://hobby-fombiclecffdgbkeemblpial.dbs.graphenedb.com:24786',
+    // username: 'admin',
+    // password: 'b.EElh3VqwOSiP.gXiD9H3gtLuKa9MJ'
 };
 const driver = neo4j.driver(
     neo.uri,
