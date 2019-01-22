534146be9a2636083c683077694451894b4d4626
Whrabbit <whrabbitc@gmail.com>
2019-01-22 19:44:20 +0100

    changed env

--- config/env/env.js
+++ config/env/env.js
@@ -2,13 +2,15 @@ var env = {
     webPort: process.env.PORT || 3040,
     dbHost: process.env.DB_HOST || 'localhost',
     dbPort: process.env.DB_PORT || '',
-    dbUser: process.env.DB_USER || '',
+    dbUser: process.env.DB_USER || 'admin',
     dbPassword: process.env.DB_PASSWORD || '',
-    dbDatabase: process.env.DB_DATABASE || 'raidgo'
+    dbDatabase: process.env.DB_DATABASE || 'raidgo',
+    dbMongo: '@ds129386.mlab.com:29386/raidgo'
 }
 
 var dburl = process.env.NODE_ENV === 'production' ?
-    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
+    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbMongo :
+    // 'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
     'mongodb://localhost/' + env.dbDatabase
 
 module.exports = {
