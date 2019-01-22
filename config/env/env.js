38305e3015f55baa9c6dc5e66ec45a52ecaa212d
Whrabbit <whrabbitc@gmail.com>
2019-01-22 19:49:00 +0100

    changed

--- config/env/env.js
+++ config/env/env.js
@@ -2,8 +2,8 @@ var env = {
     webPort: process.env.PORT || 3040,
     dbHost: process.env.DB_HOST || 'localhost',
     dbPort: process.env.DB_PORT || '',
-    dbUser: process.env.DB_USER || 'admin',
-    dbPassword: process.env.DB_PASSWORD || '',
+    dbUser: process.env.DB_USER || 'Whrabbit',
+    dbPassword: process.env.DB_PASSWORD || 'test123',
     dbDatabase: process.env.DB_DATABASE || 'raidgo',
     dbMongo: '@ds129386.mlab.com:29386/raidgo'
 }
